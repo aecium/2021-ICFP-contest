6873a29b91e4286d6050bc458acbef08fd38a2f3
aecium <aecium@gmail.com>
2021-07-09 16:51:34 -0500

    the submit.sh will stop you from submiting a solution if it has been less than 305 seconds. this is local not global tracked

--- tools/submit.sh
+++ tools/submit.sh
@@ -6,15 +6,30 @@ source .env
 
 solutions_path=../solutions
 
-solutions=$(ls $solutions_path)
+solutions=$(ls $solutions_path/*.json)
 
 for solution in $solutions; do
-    echo "submitting: ${solution}"
-    curl https://poses.live/api/problems/$(basename --suffix=.json $solution)/solutions \
-    --request POST -g \
-    -H "Accept: application/json" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer ${APIKEY}" \
-    --data "$(cat ${solutions_path}/${solution})"
+    solution_id=$(basename --suffix=.json $solution)
+
+    if [ -f  ${solutions_path}/${solution_id}.lastSent ]; then
+        last_sent=$(($(date +%s) - $(date +%s -r ${solutions_path}/${solution_id}.lastSent)))
+    else
+        last_sent=999
+    fi
+
+    if [ ${last_sent} -ge 305 ]; then
+        echo -e "submitting: ${solution}"
+        curl https://poses.live/api/problems/${solution_id}/solutions \
+        --request POST -g \
+        -H "Accept: application/json" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer ${APIKEY}" \
+        --data "$(cat ${solutions_path}/${solution})" && \
+        touch ${solutions_path}/${solution_id}.lastSent
+    else
+        echo -e "\nHold your hourses there buddy!"
+        echo "Too soon to submit: ${solution}"
+        echo -e "Waite another $(( 305 - ${last_sent} )) seconds\n"
+    fi
 done
 echo Done
